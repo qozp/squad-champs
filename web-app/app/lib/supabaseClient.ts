d0e481d59ca031d498b6d49008c4bacc421d4659
Qozp <isbpham@gmail.com>
2025-10-25 22:16:05 -0400

    test

--- web-app/app/lib/supabaseClient.ts
+++ web-app/app/lib/supabaseClient.ts
@@ -3,14 +3,18 @@ import { createClient, type SupabaseClient } from "@supabase/supabase-js";
 
 let supabase: SupabaseClient | null = null;
 
+const test_URL = "https://ffenletsvfvbqvdbgdow.supabase.co"
+const test_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmZW5sZXRzdmZ2YnF2ZGJnZG93Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk5Njk5NDUsImV4cCI6MjA3NTU0NTk0NX0.dA1JCV2uDOkn10NqzHGbALJhZvm49Xwh9vwDeEC7ylk"
+
+
 // Function to initialize client
 export const getSupabaseClient = (env?: any) => {
     if (!supabase) {
         // Local dev uses Vite env
-        const url =
-            (env?.SUPABASE_URL as string) ?? import.meta.env.VITE_SUPABASE_URL;
-        const key =
-            (env?.SUPABASE_KEY as string) ?? import.meta.env.VITE_SUPABASE_ANON_KEY;
+        const url = test_URL
+        // (env?.SUPABASE_URL as string) ?? import.meta.env.VITE_SUPABASE_URL;
+        const key = test_ANON_KEY
+        // (env?.SUPABASE_KEY as string) ?? import.meta.env.VITE_SUPABASE_ANON_KEY;
 
         if (!url || !key) {
             throw new Error(

094af41c9ffa53d4ed1cab4942fee3044226b297
Qozp <isbpham@gmail.com>
2025-10-27 20:16:59 -0400

    dark mode change

--- web-app/app/routes/squad.tsx
+++ web-app/app/routes/squad.tsx
@@ -7,7 +7,7 @@ export function meta() {
   ];
 }
 
-function SquadPage() {
+export default function SquadPage() {
   return (
     <div className="min-h-screen text-foreground">
       <section className="container mx-auto px-4 py-16">

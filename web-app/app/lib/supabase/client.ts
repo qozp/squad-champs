6bc491a8d86c1a7c3191631d65b810044323f0e5
Qozp <isbpham@gmail.com>
2025-10-28 21:25:55 -0400

    prod fix

--- web-app/app/lib/supabase/client.ts
+++ web-app/app/lib/supabase/client.ts
@@ -1,7 +1,10 @@
 import { createBrowserClient } from '@supabase/ssr'
 
-const VITE_SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL!
-const VITE_SUPABASE_PUBLISHABLE_OR_ANON_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_OR_ANON_KEY!
+const VITE_SUPABASE_URL = "https://ffenletsvfvbqvdbgdow.supabase.co"
+const VITE_SUPABASE_PUBLISHABLE_OR_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmZW5sZXRzdmZ2YnF2ZGJnZG93Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk5Njk5NDUsImV4cCI6MjA3NTU0NTk0NX0.dA1JCV2uDOkn10NqzHGbALJhZvm49Xwh9vwDeEC7ylk"
+
+// const VITE_SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL!
+// const VITE_SUPABASE_PUBLISHABLE_OR_ANON_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_OR_ANON_KEY!
 
 
 export function createClient() {

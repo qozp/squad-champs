9ab36618616cbb6160fc6d53d9c294eb6cc04e61
Qozp <isbpham@gmail.com>
2025-10-28 20:55:41 -0400

    bugfix

--- web-app/app/lib/supabase/server.ts
+++ web-app/app/lib/supabase/server.ts
@@ -8,7 +8,7 @@ export function createSupabaseClient(request: Request, context?: any) {
   const VITE_SUPABASE_PUBLISHABLE_OR_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmZW5sZXRzdmZ2YnF2ZGJnZG93Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk5Njk5NDUsImV4cCI6MjA3NTU0NTk0NX0.dA1JCV2uDOkn10NqzHGbALJhZvm49Xwh9vwDeEC7ylk"
 
 
-  const supabase = createServerClient(VITE_SUPABASE_ANON_KEY, VITE_SUPABASE_PUBLISHABLE_OR_ANON_KEY
+  const supabase = createServerClient(VITE_SUPABASE_ANON_KEY, VITE_SUPABASE_PUBLISHABLE_OR_ANON_KEY,
   // process.env.VITE_SUPABASE_URL!,
   // process.env.VITE_SUPABASE_PUBLISHABLE_OR_ANON_KEY!,
     {

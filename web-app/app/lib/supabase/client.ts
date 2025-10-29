3763cc69c6eb7f132bedb7c2b34d44ab47abe07f
Qozp <isbpham@gmail.com>
2025-10-28 21:10:18 -0400

    test 3 hardcoding

--- web-app/app/lib/supabase/client.ts
+++ web-app/app/lib/supabase/client.ts
@@ -1,8 +1,12 @@
 import { createBrowserClient } from '@supabase/ssr'
 
+const VITE_SUPABASE_URL = "https://ffenletsvfvbqvdbgdow.supabase.co"
+const VITE_SUPABASE_PUBLISHABLE_OR_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmZW5sZXRzdmZ2YnF2ZGJnZG93Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk5Njk5NDUsImV4cCI6MjA3NTU0NTk0NX0.dA1JCV2uDOkn10NqzHGbALJhZvm49Xwh9vwDeEC7ylk"
+
+
 export function createClient() {
   return createBrowserClient(
-    process.env.VITE_SUPABASE_URL!,
-    process.env.VITE_SUPABASE_PUBLISHABLE_OR_ANON_KEY!,
+    VITE_SUPABASE_URL,
+    VITE_SUPABASE_PUBLISHABLE_OR_ANON_KEY
   )
 }

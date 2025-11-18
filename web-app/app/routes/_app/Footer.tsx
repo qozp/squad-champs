640525b69e24efeab74171d75c316f1f21d79037
Qozp <isbpham@gmail.com>
2025-11-18 02:00:10 -0500

    add contact us to footer

--- web-app/app/routes/_app/Footer.tsx
+++ web-app/app/routes/_app/Footer.tsx
@@ -1,12 +1,15 @@
+import { Mail } from "lucide-react";
+
 export default function Footer() {
   return (
     <footer className="flex flex-row justify-center items-center gap-4 py-4 text-center text-sm text-foreground">
       <div className="flex flex-row gap-4 items-center">
         <a
-          href="https://squadchamps.com"
-          className="hover:underline font-medium"
+          href="mailto:contact@squadchamps.com"
+          className="hover:underline font-medium flex items-center gap-1"
         >
-          Main Site
+          <Mail className="h-4 w-4" />
+          Contact Us
         </a>
         {/* <a
             href="https://squadchamps.com/blog"

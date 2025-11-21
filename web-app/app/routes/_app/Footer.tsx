4e2594374c77d4a5227b37fc8725e0c9f9cd2977
Qozp <isbpham@gmail.com>
2025-11-21 14:20:29 -0500

    github added to footer

--- web-app/app/routes/_app/Footer.tsx
+++ web-app/app/routes/_app/Footer.tsx
@@ -1,31 +1,26 @@
-import { Mail } from "lucide-react";
+import { Github, Mail } from "lucide-react";
 
 export default function Footer() {
   return (
     <footer className="flex flex-row justify-center items-center gap-4 py-4 text-center text-sm text-foreground">
       <div className="flex flex-row gap-4 items-center">
-        <span className="flex flex-row gap-x-2">
-          Have any feedback?
-          <a
-            href="mailto:contact@squadchamps.com"
-            className="hover:underline font-medium flex items-center gap-1"
-          >
-            <Mail className="h-4 w-4" />
-            Contact Us
-          </a>
-        </span>
-        {/* <a
-            href="https://squadchamps.com/blog"
-            className="hover:underline font-medium"
-          >
-            Blog
-          </a>
-          <a
-            href="https://squadchamps.com/careers"
-            className="hover:underline font-medium"
-          >
-            Careers
-          </a> */}
+        <a
+          href="mailto:contact@squadchamps.com"
+          className="hover:underline font-medium flex items-center gap-1"
+        >
+          <Mail className="h-4 w-4" />
+          Contact Us
+        </a>
+
+        <a
+          href="https://github.com/qozp/squad-champs"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:underline font-medium flex items-center gap-1"
+        >
+          <Github className="h-4 w-4" />
+          Source Code
+        </a>
       </div>
       <span className="">&copy; {new Date().getFullYear()} Squad Champs</span>
     </footer>

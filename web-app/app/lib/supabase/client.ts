1f90d624d227497cce80413c811612df23b62adb
Qozp <isbpham@gmail.com>
2025-11-21 01:52:20 -0500

    revert

--- web-app/app/lib/supabase/client.ts
+++ web-app/app/lib/supabase/client.ts
@@ -1,11 +1,7 @@
 import { createBrowserClient } from '@supabase/ssr'
-import { env } from 'cloudflare:workers';
 
-// const VITE_SUPABASE_URL = "https://ffenletsvfvbqvdbgdow.supabase.co"
-// const VITE_SUPABASE_PUBLISHABLE_OR_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmZW5sZXRzdmZ2YnF2ZGJnZG93Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk5Njk5NDUsImV4cCI6MjA3NTU0NTk0NX0.dA1JCV2uDOkn10NqzHGbALJhZvm49Xwh9vwDeEC7ylk"
-
-const VITE_SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const VITE_SUPABASE_PUBLISHABLE_OR_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const VITE_SUPABASE_URL = "https://ffenletsvfvbqvdbgdow.supabase.co"
+const VITE_SUPABASE_PUBLISHABLE_OR_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmZW5sZXRzdmZ2YnF2ZGJnZG93Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk5Njk5NDUsImV4cCI6MjA3NTU0NTk0NX0.dA1JCV2uDOkn10NqzHGbALJhZvm49Xwh9vwDeEC7ylk"
 
 export function createClient() {
   return createBrowserClient(
